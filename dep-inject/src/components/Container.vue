06bc6a41d77ee2ec3724fc04bac5e755fc9b3163
Qingyu Deng <i@ayase-lab.com>
2021-10-14 20:06:23 +0800

    update

---
+++ dep-inject/src/components/Container.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, getCurrentInstance, reactive, useContext } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const instance = getCurrentInstance()
+console.log(instance)
+
+
+const result = []
+function appendResult(e) {
+  console(e)
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ dep-inject/src/components/Step.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="onClick">click</button>
+  </div>
+</template>
+
+<script setup>
+import { defineEmit, defineProps, reactive, useContext } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+defineEmit(["next"])
+
+const context = useContext()
+function onClick() {
+  context.parent.emit("next", "hello world")
+}
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
